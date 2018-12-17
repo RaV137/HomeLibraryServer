6d61288a549e6a300d7f322b8da9ab4d8dbd6eee
RaV137 <rafalek96@gmail.com>
2018-12-17 01:02:07 +0100

    nowy insert w skrypcie

--- src/main/resources/db/mock_data/books_insert.sql
+++ src/main/resources/db/mock_data/books_insert.sql
@@ -8,4 +8,9 @@ INSERT INTO hls.books(
 	'http://books.google.com/books/content?id=djJNDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE72bGizr9FvwNOZiQ3PCxBWOYBYKTa07_jdsM37Lo1GFd-W6ePZE0wGT_TCb2FfAHusEc8AV-YdfGHJNEacOGfmQ4lUIL6r-yJoHOT6FK02g1RUGXOzMn9oJhz7a5L021lj6xJfn&source=gbs_api',
 	'djJNDwAAQBAJ');
 
+INSERT INTO hls.books(
+	title, author, id_user, id_room, shelf, rating, favourite, is_read, thumbnail, id_google_books_api)
+	VALUES ('Mort', 'Terry Pratchett', 1, 1, 3, 8, true, true,
+	'http://books.google.com/books/content?id=R0Aw51Xmci4C&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE70Z7fYi4YrhdzPc3h3Y4CWpXr0FWYVcVDjMcz4jbNQvnIQ9H3QGHVyGelLZyefgVy0AOHYcUnEZgbAsFhKURFybxG8nPZRH9yvN0SqKokpYG_n8U-eAPkrsc7M7JPpq41CtGetg&source=gbs_api',
+	'R0Aw51Xmci4C');
 
